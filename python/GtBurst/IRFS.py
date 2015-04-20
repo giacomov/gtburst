62e88dec1de17404f5b3546f7b415c1be460aaf7
Giacomo Vianello <giacomov@slac.stanford.edu>
2015-04-20 13:56:33 -0700

    Added the public names of the templates for P8 data

--- python/GtBurst/IRFS.py
+++ python/GtBurst/IRFS.py
@@ -114,20 +114,20 @@ IRFS['P7REP_ULTRACLEAN']  = IRF('P7REP_ULTRACLEAN','P7REP_ULTRACLEAN_V15','202,2
 
 
 #Pass 8 final (?)
-galactic='template_4years_P8_V2_scaled.fits'
+galactic='template_4years_P8_V2_scaled.fits, gll_iem_v06.fits'
 
-IRFS['P8_TRANSIENT100E']    = IRF('P8_TRANSIENT100E' ,'P8R2_TRANSIENT100E_V6' ,'302',     2, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt') # YOU SHOULD USE THE BKGE FOR THIS 
-IRFS['P8_TRANSIENT100']     = IRF('P8_TRANSIENT100' ,'P8R2_TRANSIENT100_V6'  ,'302',     4, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt') # YOU SHOULD USE THE BKGE FOR THIS 
-IRFS['P8_TRANSIENT020E']    = IRF('P8_TRANSIENT020E' ,'P8R2_TRANSIENT020E_V6' ,'302',     8, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt')
-IRFS['P8_TRANSIENT020']     = IRF('P8_TRANSIENT020' ,'P8R2_TRANSIENT020_V6'  ,'302',    16, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt')
-IRFS['P8_TRANSIENT010E']    = IRF('P8_TRANSIENT010E' ,'P8R2_TRANSIENT010E_V6' ,'302',    32, galactic, 'isotropic_transient_r010_4years_P8V4_rev3.txt')
-IRFS['P8_TRANSIENT010']     = IRF('P8_TRANSIENT010' ,'P8R2_TRANSIENT010_V6'  ,'302',    64, galactic, 'isotropic_transient_r010_4years_P8V4_rev3.txt')
-IRFS['P8_SOURCE']           = IRF('P8_SOURCE'        ,'P8R2_SOURCE_V6'       ,'302',   128, galactic, 'isotropic_source_4years_P8V3.txt')
-IRFS['P8_CLEAN']            = IRF('P8_CLEAN'         ,'P8R2_CLEAN_V6'       ,'302',   256, galactic, 'isotropic_clean_4years_P8V3.txt')
-IRFS['P8_ULTRACLEAN']       = IRF('P8_ULTRACLEAN' ,'P8R2_ULTRACLEAN_V6'    ,'302',   512, galactic, 'isotropic_ultraclean_4years_P8V3.txt')
-IRFS['P8_ULTRACLEANVETO']   = IRF('P8_ULTRACLEANVETO','P8R2_ULTRACLEANVETO_V6','302',  1024, galactic, 'isotropic_ultraclean_4years_P8V3.txt')
-IRFS['P8_TRANSIENT100S']    = IRF('P8_TRANSIENT100S' ,'P8R2_TRANSIENT100S_V6' ,'302', 32768, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt') # YOU SHOULD USE THE BKGE FOR THIS 
-IRFS['P8_TRANSIENT015S']    = IRF('P8_TRANSIENT015S' ,'P8R2_TRANSIENT015S_V6' ,'302', 65536, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt')
+IRFS['P8_TRANSIENT100E']    = IRF('P8_TRANSIENT100E' ,'P8R2_TRANSIENT100E_V6' ,'302',     2, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT020_V6_v06.txt') # YOU SHOULD USE THE BKGE FOR THIS 
+IRFS['P8_TRANSIENT100']     = IRF('P8_TRANSIENT100' ,'P8R2_TRANSIENT100_V6'  ,'302',     4, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT020_V6_v06.txt') # YOU SHOULD USE THE BKGE FOR THIS 
+IRFS['P8_TRANSIENT020E']    = IRF('P8_TRANSIENT020E' ,'P8R2_TRANSIENT020E_V6' ,'302',     8, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT020E_V6_v06.txt')
+IRFS['P8_TRANSIENT020']     = IRF('P8_TRANSIENT020' ,'P8R2_TRANSIENT020_V6'  ,'302',    16, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT020_V6_v06.txt')
+IRFS['P8_TRANSIENT010E']    = IRF('P8_TRANSIENT010E' ,'P8R2_TRANSIENT010E_V6' ,'302',    32, galactic, 'isotropic_transient_r010_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT010E_V6_v06.txt')
+IRFS['P8_TRANSIENT010']     = IRF('P8_TRANSIENT010' ,'P8R2_TRANSIENT010_V6'  ,'302',    64, galactic, 'isotropic_transient_r010_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT010E_V6_v06.txt')
+IRFS['P8_SOURCE']           = IRF('P8_SOURCE'        ,'P8R2_SOURCE_V6'       ,'302',   128, galactic, 'isotropic_source_4years_P8V3.txt, iso_P8R2_SOURCE_V6_v06.txt')
+IRFS['P8_CLEAN']            = IRF('P8_CLEAN'         ,'P8R2_CLEAN_V6'       ,'302',   256, galactic, 'isotropic_clean_4years_P8V3.txt, iso_P8R2_CLEAN_V6_v06.txt')
+IRFS['P8_ULTRACLEAN']       = IRF('P8_ULTRACLEAN' ,'P8R2_ULTRACLEAN_V6'    ,'302',   512, galactic, 'isotropic_ultraclean_4years_P8V3.txt, iso_P8R2_ULTRACLEAN_V6_v06.txt')
+IRFS['P8_ULTRACLEANVETO']   = IRF('P8_ULTRACLEANVETO','P8R2_ULTRACLEANVETO_V6','302',  1024, galactic, 'isotropic_ultraclean_4years_P8V3.txt, iso_P8R2_ULTRACLEANVETO_V6_v06.txt')
+IRFS['P8_TRANSIENT100S']    = IRF('P8_TRANSIENT100S' ,'P8R2_TRANSIENT100S_V6' ,'302', 32768, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT0100S_V6_v06.txt') # YOU SHOULD USE THE BKGE FOR THIS 
+IRFS['P8_TRANSIENT015S']    = IRF('P8_TRANSIENT015S' ,'P8R2_TRANSIENT015S_V6' ,'302', 65536, galactic, 'isotropic_transient_r020_4years_P8V4_rev3.txt, iso_P8R2_TRANSIENT015S_V6_v06.txt')
 
 PROCS                         = collections.OrderedDict()
 for k,v in IRFS.iteritems():
